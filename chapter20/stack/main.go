b49ef4b9d3b166dbc30be337043cc43403b2cfe1
吴名 <wuming497735138@gmail.com>
2018-08-13 19:34:11 +0800

    code var name

--- chapter20/stack/main.go
+++ chapter20/stack/main.go
@@ -13,44 +13,44 @@ type Stack struct {
 }
 
 //入栈
-func (this *Stack) Push(val int) (err error) {
+func (stack *Stack) Push(val int) (err error) {
 
 	//先判断栈是否满了
-	if this.Top == this.MaxTop-1 {
+	if stack.Top == stack.MaxTop-1 {
 		fmt.Println("stack full")
 		return errors.New("stack full")
 	}
-	this.Top++
+	stack.Top++
 	//放入数据
-	this.arr[this.Top] = val
+	stack.arr[stack.Top] = val
 	return
 }
 
 //出栈
-func (this *Stack) Pop() (val int, err error) {
+func (stack *Stack) Pop() (val int, err error) {
 	//判断栈是否空
-	if this.Top == -1 {
+	if stack.Top == -1 {
 		fmt.Println("stack empty!")
 		return 0, errors.New("stack empty")
 	}
 
-	//先取值，再 this.Top--
-	val = this.arr[this.Top]
-	this.Top--
+	//先取值，再 stack.Top--
+	val = stack.arr[stack.Top]
+	stack.Top--
 	return val, nil
 
 }
 
 //遍历栈，注意需要从栈顶开始遍历
-func (this *Stack) List() {
+func (stack *Stack) List() {
 	//先判断栈是否为空
-	if this.Top == -1 {
+	if stack.Top == -1 {
 		fmt.Println("stack empty")
 		return
 	}
 	fmt.Println("栈的情况如下：")
-	for i := this.Top; i >= 0; i-- {
-		fmt.Printf("arr[%d]=%d\n", i, this.arr[i])
+	for i := stack.Top; i >= 0; i-- {
+		fmt.Printf("arr[%d]=%d\n", i, stack.arr[i])
 	}
 
 }
