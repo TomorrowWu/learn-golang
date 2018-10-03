3c6c4aa8bea66985eb2f99de2aa1975c5cbc8ee0
吴名 <wuming497735138@gmail.com>
2018-10-03 18:03:00 +0800

    add a Practical project Crawler

---
+++ crawler/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://www.zhenai.com/zhenghun")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: status code", resp.StatusCode)
+		return
+	}
+
+	e := determineRncoding(resp.Body)
+	//utf8Reader := transform.NewReader(resp.Body, simplifiedchinese.GBK.NewDecoder())
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	all, err := ioutil.ReadAll(utf8Reader)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", all)
+}
+
+func determineRncoding(r io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
